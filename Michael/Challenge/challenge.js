89663eebb9351391df64f69d61f7126e5dc232b7
Michael Zechner <michael-zechner@gmx.at>
2021-09-28 22:28:42 +0200

    Challenge complete

--- Michael/Challenge/challenge.js
+++ Michael/Challenge/challenge.js
@@ -4,22 +4,26 @@ var heading=document.getElementsByClassName("announcement")[0];
 heading.innerHTML=`${player1} vs ${player2}`;
 var blue=document.getElementById("mz-blue");
 var red=document.getElementById("mz-red");
-red.style.display="none";
+red.style.display="hidden";
 function roll() {
     return Math.floor(Math.random()*6+1);
 }
+var counter=1;
 var left=0;
 var right=0;
 function pOne() {
-    blue.style.display="none";
+    if (counter===1) {
+        counter=-1;
+        blue.style.display="hidden";
     left=roll();
     let result= document.getElementById("l-result");
     result.innerHTML=left;
     red.style.display="initial"
-    
+    }
 }
 function pTwo() {
-    red.style.display="none";
+    if (counter===-1) {
+        counter=1;
     right=roll();
     let result= document.getElementById("r-result");
     result.innerHTML=right;
@@ -34,6 +38,8 @@ function pTwo() {
         default:
             final.innerHTML="Draw!";
     }
+    }
+    
 }
 blue.addEventListener("click",pOne);
 red.addEventListener("click",pTwo);
